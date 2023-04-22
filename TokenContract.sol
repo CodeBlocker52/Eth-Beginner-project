f38baa4d1e995c01c4bbd641a613b814c484c959
CodeBlocker52 <131183962+CodeBlocker52@users.noreply.github.com>
2023-04-22 19:42:20 +0530

    Update TokenContract.sol

--- TokenContract.sol
+++ TokenContract.sol
@@ -3,32 +3,24 @@
 pragma solidity ^0.8.17;
 
 contract MyToken {
-    string public name; // token name
-    string public symbol; // token abbreviation
-    uint256 public totalSupply; // total token supply
+    string public tokenName="Meta"; 
+    string public tokenAbbrv="MTA"; 
+    uint256 public totalSupply=0 ; 
+    
+    mapping(address => uint) public balances; 
 
-    mapping(address => uint256) public balanceOf; // balance of each account
-
-    event Mint(address indexed to, uint256 value);
-    event Burn(address indexed from, uint256 value);
-
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _totalSupply;
-        balanceOf[msg.sender] = _totalSupply;
-    }
-
-    function mint(address _to, uint256 _value) public {
+   
+    function mint(address _address, uint256 _value) public {
         totalSupply += _value;
-        balanceOf[_to] += _value;
-        emit Mint(_to, _value);
+        balances[_address] += _value;
+        
+    }
+      function burn(address _address, uint256 _value) public {
+        totalSupply -= _value;
+        balances[_address] -= _value;
+        
     }
 
-    function burn(address _from, uint256 _value) public {
-        require(balanceOf[_from] >= _value, "Insufficient balance");
-        totalSupply -= _value;
-        balanceOf[_from] -= _value;
-        emit Burn(_from, _value);
-    }
+
+   
 }
